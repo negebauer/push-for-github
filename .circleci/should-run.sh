6e888f0989f8df98fa8fd4b14896fd8b0077e4ce
Nicolás Gebauer <negebauerm@gmail.com>
2019-03-10 05:07:13 -0300

    sgould run 3

--- .circleci/should-run.sh
+++ .circleci/should-run.sh
@@ -1,7 +1,7 @@
 package_folder=$1
 modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
 
-if [[ $modified_files == packages/$package_folder* ]]; then
+if [[ $modified_files == *packages/$package_folder/* ]]; then
   echo "Modified files for package $package_folder, continue"
 else
   echo "No modified files for package $package_folder, stopping"
