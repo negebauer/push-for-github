e80da8d44ff3de45e7425f83917580cd29cbdb5e
Nicolás Gebauer <negebauerm@gmail.com>
2019-03-11 12:02:49 -0300

    chore: dont run jobs on chore commands
    except for chore(docker), chore(deploy) or chore(ci) commits
    this one should've been chore(ci), but avoided the (ci) to test the commit

--- .circleci/should-run.sh
+++ .circleci/should-run.sh
@@ -1,9 +1,25 @@
 package_folder=$1
 modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
+commit_message=$(git show -s --format=%B)
 
-if [[ $modified_files == *packages/$package_folder/* ]]; then
-  echo "Modified files for package $package_folder, continue"
-else
-  echo "No modified files for package $package_folder, stopping"
+stop() {
   circleci step halt
+  return 0
+}
+
+if [[ ! $modified_files == *packages/$package_folder/* ]]; then
+  echo "No modified files for package $package_folder, stopping"
+  stop
+fi
+
+echo "Modified files for package $package_folder, continue"
+
+if [[ $commit_message == chore* ]]; then
+  if [[ ! $commit_message == chore\(docker ]] \
+    || [[ ! $commit_message == chore\(deploy ]] \
+    || [[ ! $commit_message == chore\(ci ]]
+    then
+    echo "Chore commit, stopping"
+    stop
+  fi
 fi
