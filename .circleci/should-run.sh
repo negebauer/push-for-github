cd4338a78cae18bc66a43f9a4b1b84ec0244ed04
Nicolás Gebauer <negebauerm@gmail.com>
2019-05-23 12:47:44 -0400

    chore(ci): improve should-run command (#11)
    Define PACKAGE env in config for each package steps

--- .circleci/should-run.sh
+++ .circleci/should-run.sh
@@ -1,25 +1,37 @@
-package_folder=$1
+package=$PACKAGE
 modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
 commit_message=$(git show -s --format=%B)
 
 stop() {
   circleci step halt
-  return 0
+  exit 0
 }
 
-if [[ ! $modified_files == *packages/$package_folder/* ]]; then
-  echo "No modified files for package $package_folder, stopping"
-  stop
+error() {
+  exit 1
+}
+
+echo "Checking valid PACKAGE env..."
+if [[ -z $package ]]; then
+  echo "no PACKAGE env for this step, fix this!"
+  error
 fi
 
-echo "Modified files for package $package_folder, continue"
-
+echo "Checking if chore commit..."
 if [[ $commit_message == chore* ]]; then
   if [[ ! $commit_message == chore\(docker ]] \
-    || [[ ! $commit_message == chore\(deploy ]] \
-    || [[ ! $commit_message == chore\(ci ]]
+    && [[ ! $commit_message == chore\(deploy ]] \
+    && [[ ! $commit_message == chore\(ci ]]
     then
     echo "Chore commit, stopping"
     stop
   fi
 fi
+
+echo "Checking if package $PACKAGE changed..."
+if [[ ! $modified_files == *packages/$package/* ]]; then
+  echo "No modified files for package $package, stopping"
+  stop
+fi
+
+echo "Modified files for package $package, continue"
