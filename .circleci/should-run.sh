ca65343e5398eb0305692d511eeb23c6967c767d
Nicolás Gebauer <negebauerm@gmail.com>
2019-05-23 12:26:38 -0400

    test

--- .circleci/should-run.sh
+++ .circleci/should-run.sh
@@ -1,4 +1,4 @@
-package_folder=$1
+package_folder=$SHOULD_RUN_PACKAGE
 modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
 commit_message=$(git show -s --format=%B)
 
@@ -6,6 +6,11 @@ stop() {
   circleci step halt
   exit 0
 }
+
+if [[ $package_folder == "NONE" ]]; then
+  echo "no SHOULD_RUN_PACKAGE env for this step, fix this!"
+  stop
+fi
 
 if [[ $commit_message == chore* ]]; then
   if [[ ! $commit_message == chore\(docker ]] \
