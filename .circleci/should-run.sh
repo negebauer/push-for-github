b013ae880c171a796ab6a7ce375cde7205d92f54
Nicolás Gebauer <negebauerm@gmail.com>
2019-05-23 12:11:14 -0400

    test ci update

--- .circleci/should-run.sh
+++ .circleci/should-run.sh
@@ -4,8 +4,18 @@ commit_message=$(git show -s --format=%B)
 
 stop() {
   circleci step halt
-  return 0
+  exit 0
 }
+
+if [[ $commit_message == chore* ]]; then
+  if [[ ! $commit_message == chore\(docker ]] \
+    && [[ ! $commit_message == chore\(deploy ]] \
+    && [[ ! $commit_message == chore\(ci ]]
+    then
+    echo "Chore commit, stopping"
+    stop
+  fi
+fi
 
 if [[ ! $modified_files == *packages/$package_folder/* ]]; then
   echo "No modified files for package $package_folder, stopping"
@@ -13,13 +23,3 @@ if [[ ! $modified_files == *packages/$package_folder/* ]]; then
 fi
 
 echo "Modified files for package $package_folder, continue"
-
-if [[ $commit_message == chore* ]]; then
-  if [[ ! $commit_message == chore\(docker ]] \
-    || [[ ! $commit_message == chore\(deploy ]] \
-    || [[ ! $commit_message == chore\(ci ]]
-    then
-    echo "Chore commit, stopping"
-    stop
-  fi
-fi
